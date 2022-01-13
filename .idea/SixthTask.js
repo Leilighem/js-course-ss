928610451a6dcab2870742214c830869442111cf
Daria Khomenko <daria.khomenko@partner.cgm.com>
2022-01-14 00:02:58 +0200

    First files

---
+++ .idea/SixthTask.js
@@ -0,0 +1,18 @@
+//Here will be the sixth task code
+let myName = 'Daria'; //This is a string variable
+let mySurname = 'Khomenko'; //This is a string variable
+let myGroup = 'QA training SoftServe 2022'; //This is a string variable
+let myYear = 1989; //This is a numeric variable
+let isMarried = true; //This is a boolean variable
+
+console.log(typeof myYear); // "numeric"
+console.log(typeof isMarried); // "boolean"
+console.log(typeof myName); // "string"
+console.log(typeof mySurname); // "string"
+console.log(typeof myGroup); // "string"
+
+let someVariable; //undefined
+let someNullVariable = null; //null
+
+console.log(typeof someVariable); // "undefined"
+console.log(typeof someNullVariable); // "null"
